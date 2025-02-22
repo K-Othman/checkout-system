de0b8e57e479ae44ec8453b3737b5cd4e0ab8a8f
K-Othman <karim_othman88@hotmail.com>
2025-02-22 23:36:33 +0000

    added items button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">HI</div>;
+  return (
+    <main className="App">
+      <div>
+        <h2>Checkout System</h2>
+        <p>Select items to add to your basket:</p>
+        <div>
+          <button>Add A</button>
+          <button>Add B</button>
+          <button>Add C</button>
+          <button>Add D</button>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default App;
