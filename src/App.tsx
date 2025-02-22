fa978a60c808f8c2d48975a93bfa807cefc998ea
K-Othman <karim_othman88@hotmail.com>
2025-02-22 23:53:45 +0000

    created additem function and added it to the buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [basket, setBasket] = useState<{ [key: string]: number }>({});
+  const [total, setTotal] = useState<number>(0);
+
+  // Adding items to the basket
+  const addItem = (item: string) => {
+    setBasket((prevBasket) => {
+      const newBasket = { ...prevBasket, [item]: prevBasket[item] + 1 };
+      console.log(newBasket);
+
+      return newBasket;
+    });
+  };
+
   return (
     <main className="App">
       <div>
         <h2>Checkout System</h2>
         <p>Select items to add to your basket:</p>
         <div>
-          <button>Add A</button>
-          <button>Add B</button>
-          <button>Add C</button>
-          <button>Add D</button>
+          <button onClick={() => addItem("A")}>Add A</button>
+          <button onClick={() => addItem("B")}>Add B</button>
+          <button onClick={() => addItem("C")}>Add C</button>
+          <button onClick={() => addItem("D")}>Add D</button>
+        </div>
+        <div>
+          <h3>Basket:</h3>
         </div>
       </div>
     </main>
