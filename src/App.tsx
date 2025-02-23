c19a41f225476539d4ee86315cf384fb529d5b15
K-Othman <karim_othman88@hotmail.com>
2025-02-23 22:48:50 +0000

    styled all the page and changed the layout using flexbox to make it clearer

--- src/App.tsx
+++ src/App.tsx
@@ -16,16 +16,16 @@ function App() {
     });
   };
 
-  // my remove item with a counter
+  // removing item with a counter
   const removeItem = (item: string) => {
     setBasket((prevBasket) => {
-      if (!prevBasket[item]) return prevBasket; // If item isn't in the basket, do nothing
+      if (!prevBasket[item]) return prevBasket;
 
       const newBasket = { ...prevBasket };
       newBasket[item]--;
 
       if (newBasket[item] === 0) {
-        delete newBasket[item]; // Remove item if count reaches 0
+        delete newBasket[item];
       }
 
       setTotal(calculateTotal(newBasket));
@@ -33,49 +33,55 @@ function App() {
     });
   };
 
-  // Gitting the [item : quantity] of basket
-  const items = Object.entries(basket);
-
   // Getting the items' key
   const itemName = Object.keys(pricingRules);
 
   return (
     <main className="App">
-      <div>
-        <h2>Checkout System</h2>
-        <p>Select items to add to your basket:</p>
+      <div className="header">
+        <h2 className="header">Checkout System</h2>
+        <p className=""> Your basket:</p>
 
         <div className="items">
           {itemName.map((item) => {
-            const product = pricingRules[item]; // Get item details
-            const quantity = basket[item] || 0; // Get current quantity
+            // Get item details
+            const product = pricingRules[item];
 
+            // Get current quantity
+            const quantity = basket[item] || 0;
             return (
               <div key={item} className="item-container">
-                <h3>{item}</h3>
-                <p>Price: £{(product.unitPrice / 100).toFixed(2)}</p>
-
-                {product.specialPrice && (
-                  <p>
-                    Buy {product.specialPrice.quantity} for £
-                    {(product.specialPrice.price / 100).toFixed(2)}
-                  </p>
-                )}
-
-                {/* Counter Section */}
-                <div className="counter">
-                  <button
-                    onClick={() => removeItem(item)}
-                    disabled={quantity === 0}
-                  >
-                    -
-                  </button>
-                  <span>{quantity}</span>
-                  <button onClick={() => addItem(item)}>+</button>
+                <div className="name-section">
+                  <h3>Product {item}</h3>
+                  <div className="prices">
+                    <p> Price: £{(product.unitPrice / 100).toFixed(2)}</p>
+                    {product.specialPrice && (
+                      <p>
+                        Buy {product.specialPrice.quantity} for £
+                        {(product.specialPrice.price / 100).toFixed(2)}
+                      </p>
+                    )}
+                  </div>
+                </div>
+                <div>
+                  {/* Counter Section */}
+                  <div className="counter">
+                    <button
+                      onClick={() => removeItem(item)}
+                      disabled={quantity === 0}
+                    >
+                      -
+                    </button>
+                    <span>{quantity}</span>
+                    <button onClick={() => addItem(item)}>+</button>
+                  </div>
                 </div>
               </div>
             );
           })}
+          <p className="total-price">
+            Total: <span>£{(total / 100).toFixed(2)}</span>
+          </p>
         </div>
       </div>
     </main>
